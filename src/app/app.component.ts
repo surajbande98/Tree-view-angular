dfc5dfe95edcd1c6d309cd0df3a329dfc3d6fad2
Suraj <suraj.bande98@gmail.com>
2022-06-24 00:41:57 +0530

    Added tree data structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SampleData } from 'src/assets/sample.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tree-view';
+  sampleData = SampleData.data;
 }
---
+++ src/app/tree/tree.component.ts
@@ -0,0 +1,33 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tree',
+  templateUrl: './tree.component.html',
+  styleUrls: ['./tree.component.scss']
+})
+export class TreeComponent implements OnInit {
+
+  @Input() items:any;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onexpand(item:any) {
+    if (item.expanded) {
+      item.expanded = !item.expanded;
+      return;
+    } else {
+      if (item.children) {
+        if (item.children.length > 0) {
+          item.expanded = true;
+        } else {
+          item.expanded = false;
+        }
+      }
+    }
+  }
+
+
+}
